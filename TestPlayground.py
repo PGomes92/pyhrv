74fa3f43f36b88a93aee0d886ffc623e1f4c90b3
Pedro Gomes <pgomes92@gmail.com>
2019-09-19 19:54:18 -0600

    Clean branch for fresh merging

--- TestPlayground.py
+++
@@ -1,7 +0,0 @@
-import pyhrv.time_domain as td
-from pyhrv import utils
-
-nn = utils.load_sample_nni()
-
-print(td.sdnn(nn))
-print(td.sdsd(nn))
--- pyhrv/report/__init__.py
+++
@@ -1,7 +0,0 @@
-# TODO add support for all lazy imports
-# Allow lazy import
-from pyhrv.report.pdf_report import PDFReport
-# from pyhrv.report.txt_report import hrv_report
-
-
-
